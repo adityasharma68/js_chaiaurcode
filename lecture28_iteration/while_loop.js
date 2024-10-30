208d7ee31536dc72d27207248eefbb4f5404380f
adityasharma68 <divyans95205@gmail.com>
2024-10-30 21:09:49 +0530

    complete lecture 28

---
+++ lecture28_iteration/while_loop.js
@@ -0,0 +1,27 @@
+// WHILE OR DO-WHILE LOOP
+
+let index = 0;
+while (index <= 20) {
+  //   console.log(`Value of index is ${index}`);
+  index = index + 2;
+}
+
+let myArray = ["Ironman", "Hulk", "Capt. America", "Thor", "Black Widow"];
+
+let arr = 0;
+while (arr < myArray.length) {
+  console.log(`Name of heroes ${myArray[arr]}`);
+  arr = arr + 1;
+}
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//DO-WHILE LOOP
+// in DO-WHILe loop has special case ther is firstly use do statement the it use While loop. 
+
+let score = 11;
+
+do {
+  console.log(`Score is ${score}`);
+  score++
+} while (score <= 10);
+
